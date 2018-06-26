c74c6ee7c61b92c26e383dff35f6a37f4585882e
Mazin Bokhari <mazin.bokhari@gmail.com>
2018-06-26 02:10:32 -0700

    dsf

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -205,7 +205,9 @@ main() {
     # Ensure that the following actions
     # are made relative to this file's path.
 
+    echo "done"
     echo "$(dirname "${BASH_SOURCE[0]}")" 
+    echo "done"
 
     cd "$(dirname "${BASH_SOURCE[0]}")" \
         || exit 1
