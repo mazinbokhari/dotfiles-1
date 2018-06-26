689457af95e822b3c9bac429d7b62ebe2925d826
Mazin Bokhari <mazin.bokhari@gmail.com>
2018-06-26 03:39:31 -0700

    Update

--- src/os/create_directories.sh
+++ src/os/create_directories.sh
@@ -8,7 +8,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 create_directories() {
 
     declare -a DIRECTORIES=(
-        "$HOME/Downloads/torrents"
+        "$HOME/Downloads/torrents",
+        "$HOME/dev"
     )
 
     for i in "${DIRECTORIES[@]}"; do
