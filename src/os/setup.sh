db57adc007bbf692e6a7cb50c65dc303682f37d1
Mazin Bokhari <mazin.bokhari@gmail.com>
2018-06-26 10:52:10 -0700

    Update

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -217,7 +217,7 @@ main() {
     # Ensure that the following actions are made relative to this file's path.
     cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-    echo 'start'
+    echo 'new start'
 
     load_utils
 
