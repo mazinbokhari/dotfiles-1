13fca025e998bddbee212cb5a09694d471705539
Mazin Bokhari <mazin.bokhari@gmail.com>
2018-06-26 03:15:00 -0700

    Update

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -257,18 +257,15 @@ main() {
     # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     if cmd_exists "git"; then
-
         if [ "$(git config --get remote.origin.url)" != "$DOTFILES_ORIGIN" ]; then
             ./initialize_git_repository.sh "$DOTFILES_ORIGIN"
         fi
 
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
         if ! $skipQuestions; then
             ./update_content.sh
         fi
-
     fi
+    echo 'fin git'
 
     # # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
